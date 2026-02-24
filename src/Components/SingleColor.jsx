77af3481b4258566a0d85707ee2e76517c9fbf56
Mbasa <mbasacokile7@gmail.com>
2026-02-24 18:25:15 +0200

    Feat: Add Clipboard API functionlaity to ech color square

--- src/Components/SingleColor.jsx
+++ src/Components/SingleColor.jsx
@@ -1,5 +1,20 @@
+import { toast } from "react-toastify";
+
 function SingleColor({ color, index }) {
   const { hex, weight } = color;
+
+  async function saveToClipboard() {
+    if (navigator.clipboard) {
+      try {
+        await navigator.clipboard.writeText(`#${hex}`);
+        toast.success("Color copied to clipboard");
+      } catch (error) {
+        toast.error("Failed to copy color to clipboard");
+      }
+    } else {
+      toast.error("Clipboard access not available");
+    }
+  }
 
   return (
     <article
@@ -7,6 +22,7 @@ function SingleColor({ color, index }) {
       style={{
         background: `#${hex}`,
       }}
+      onClick={saveToClipboard}
     >
       <p className="percent-value">{weight}%</p>
       <p className="color-value">#{hex}</p>
